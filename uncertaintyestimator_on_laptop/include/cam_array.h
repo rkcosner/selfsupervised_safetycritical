9de014d204c02aceb8b24ad1aee02f77b9f36107
rkcosner <rkcosner@caltech.edu>
2022-02-07 18:06:08 -0800

    adding laptop monitor code from uncertaintyestimator_onlaptop

---
+++ uncertaintyestimator_on_laptop/include/cam_array.h
@@ -0,0 +1,337 @@
+#include <linux/videodev2.h>
+#include <linux/v4l2-common.h>
+#include <linux/v4l2-controls.h>
+#include <linux/videodev2.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include "elas/elas_opencv_bridge.h"
+#include <thread> 
+
+
+#include "Eigen/Dense"
+#include "Eigen/Core"
+#include <eigen3/unsupported/Eigen/CXX11/Tensor>
+
+
+// Camera Array Parameters
+float baseline = 0.052; 
+float c_x = 40;
+float focus = 221.28672543;
+constexpr float postRectificationRescale = 1.;
+int rectification_shift1_2 = 14; 
+int rectification_shift2_3 = 3; 
+
+float calib_data[16] = {1*postRectificationRescale, 0, 0, -142.27140427/2,
+                        0, 1*postRectificationRescale, 0, -102.50337029/2,
+                        0, 0, 0, -focus*postRectificationRescale, 
+                        0, 0, -1.0/0.052*0.525, 0};
+
+cv::Mat Q_(4,4, CV_32FC1, calib_data);
+
+// Image Properties 
+constexpr int width = 5120;
+constexpr int height= 800;
+constexpr float img_scale = 0.25;
+const int depth = 10; 
+const int cvt_code = 46; 
+const int convert2rgb = 0;
+constexpr int img_width = int(width*img_scale);
+constexpr int frame_width = img_width / 4;
+constexpr int img_height = int(height*img_scale);
+cv::Mat disp1_rmap[2][2];
+cv::Mat disp2_rmap[2][2];
+cv::Mat disp3_rmap[2][2];
+
+// Stereo Vars 
+StereoEfficientLargeScale elas1_(0,32); 
+StereoEfficientLargeScale elas2_(0,32); 
+StereoEfficientLargeScale elas3_(0,64); 
+
+
+cv::Mat disp1_;
+cv::Mat disp2_;
+cv::Mat disp3_;
+cv::Mat disp3plusErr_; 
+cv::Mat disp3minusErr_;
+cv::Mat d3_maxErr_(img_height*postRectificationRescale, img_width/4*postRectificationRescale, CV_8U, cv::Scalar(0));
+cv::Mat disp3hat_( img_height*postRectificationRescale, img_width/4*postRectificationRescale, CV_8U, cv::Scalar(0));
+
+cv::Mat imgL_; 
+cv::Mat imgC_; 
+cv::Mat imgR_; 
+
+constexpr int err_depth = 65;
+
+
+
+void getCVimage(const sensor_msgs::Image rosImg, cv::Mat &img){
+    cv_bridge::CvImagePtr cvPtr = cv_bridge::toCvCopy(rosImg, sensor_msgs::image_encodings::TYPE_8UC1); 
+    cv::Mat(cvPtr->image).convertTo(img, CV_8UC1);
+}
+
+
+
+
+
+
+// Image Rectification Values D1
+// cv::Mat D1_cameraMatrix_left = (cv::Mat_<float>(3,3) << 200.13893387  , 0.,         140.76444227,
+//                                                 0.     ,    200.13893387,  93.37328257,
+//                                               0.        ,   0.        ,   1.     );
+// cv::Mat D1_distortionCoefficients_left = (cv::Mat_<float>(1,5) <<  -0.1255048 ,  0.19631011 , 0.    ,      0.  ,       -0.11851587);
+// cv::Mat D1_R1 = (cv::Mat_<float>(3,3) <<    0.98395951 , 0.17665223, -0.02485322,
+//                                             -0.1770544  , 0.98408644 ,-0.0150203 ,
+//                                              0.02180435 , 0.01917974 , 0.99957827  );
+// cv::Mat D1_P1 = (cv::Mat_<float>(3,4) <<    200.13893387  , 0.    ,     143.32390594 ,  0.  ,
+//                                              0.       ,  200.13893387, 103.14701462  , 0.,
+//                                              0.        ,   0.        ,   1.        ,   0.  );
+
+// cv::Mat D1_cameraMatrix_right = (cv::Mat_<float>(3,3) << 200.13893387 ,  0.    ,     136.80080128,
+//                                                    0.  ,       200.13893387, 107.47382618,
+//                                                     0.        ,   0.        ,   1.     );
+// cv::Mat D1_distortionCoefficients_right = (cv::Mat_<float>(1,5) <<  -0.07347874 , 0.05198529 , 0. ,         0.  ,        0.01706871);
+// cv::Mat D1_R2 = (cv::Mat_<float>(3,3) <<     0.98440286 , 0.17563299 ,-0.01020129,
+//                                              -0.17543106 , 0.98432462 , 0.01813819,
+//                                              0.01322704 ,-0.01606567 , 0.99978345);
+// cv::Mat D1_P2 = (cv::Mat_<float>(3,4) <<   200.13893387 ,   0.      ,    143.60647392 ,-443.56273572,
+//                                              0.     ,     200.13893387 , 103.14701462 ,   0.      ,
+//                                              0.   ,         0.  ,          1.      ,      0.    );
+
+
+
+
+cv::Mat D1_cameraMatrix_left = (cv::Mat_<float>(3,3) << 213.38477548 ,  0.,         160.38018106,
+                                               0.    ,     213.38477548 , 90.42188151,
+                                              0.        ,   0.        ,   1.     );
+cv::Mat D1_distortionCoefficients_left = (cv::Mat_<float>(1,5) <<  -0.01021809 , 0.19241332 , 0.   ,       0. ,        -0.33436766);
+cv::Mat D1_R1 = (cv::Mat_<float>(3,3) <<    0.99676252,  0.03304748 ,-0.07329633,
+                                            -0.03419278,  0.99931114, -0.01442593,
+                                            0.0727691 ,  0.01688543  ,0.99720587  );
+cv::Mat D1_P1 = (cv::Mat_<float>(3,4) <<    213.38477548 ,  0.   ,      185.98413086  , 0. ,
+                                             0.    ,     213.38477548 , 99.90508556 ,  0. ,
+                                             0.        ,   0.        ,   1.        ,   0.  );
+
+cv::Mat D1_cameraMatrix_right = (cv::Mat_<float>(3,3) << 213.38477548  , 0. ,        154.06712743,
+                                                    0. ,        213.38477548, 106.43590771,
+                                                    0.        ,   0.        ,   1.     );
+cv::Mat D1_distortionCoefficients_right = (cv::Mat_<float>(1,5) <<  0.04534914 ,-0.13212098 , 0.    ,      0.   ,       0.16346267);
+cv::Mat D1_R2 = (cv::Mat_<float>(3,3) <<      0.9959765 ,  0.03299123, -0.08332097,
+                                             -0.03168035,  0.99935336 , 0.01700663,
+                                               0.08382816 ,-0.01429856 , 0.99637763 );
+cv::Mat D1_P2 = (cv::Mat_<float>(3,4) <<   213.38477548  ,  0.  ,        182.41349792 ,-497.26944683,
+                                              0.   ,       213.38477548  , 99.90508556 ,   0.       ,
+                                             0.   ,         0.  ,          1.      ,      0.    );
+
+
+
+
+
+
+// Image Rectification Values D3
+cv::Mat D2_cameraMatrix_left = (cv::Mat_<float>(3,3) << 216.85273609 ,  0.    ,     154.40694161,
+                                              0.  ,       216.85273609 ,105.71968046,
+                                              0.        ,   0.        ,   1.     );
+cv::Mat D2_distortionCoefficients_left = (cv::Mat_<float>(1,5) <<  0.02738416, -0.05257071 , 0.,          0. ,         0.07259553);
+cv::Mat D2_R1 = (cv::Mat_<float>(3,3) <<       0.99911503, -0.00219649 , 0.04200392,
+                                             0.00229048  ,0.99999498, -0.00218977,
+                                            -0.0419989  , 0.00228404 , 0.99911505 );
+cv::Mat D2_P1 = (cv::Mat_<float>(3,4) <<      216.85273609 ,  0.    ,     140.92088318 ,  0.  , 
+                                              0.  ,       216.85273609 ,105.3395071  ,  0.   ,
+                                             0.        ,   0.        ,   1.        ,   0.  );
+
+cv::Mat D2_cameraMatrix_right = (cv::Mat_<float>(3,3) <<216.85273609  , 0.     ,    152.07872685,
+                                                   0.     ,    216.85273609 ,104.04086618,
+                                                    0.        ,   0.        ,   1.     );
+cv::Mat D2_distortionCoefficients_right = (cv::Mat_<float>(1,5) << -0.01407029,  0.12369421 , 0.,          0.  ,       -0.19781305);
+cv::Mat D2_R2 = (cv::Mat_<float>(3,3) <<       0.99903657 , 0.00484119,  0.04361749,
+                                            -0.00493879 , 0.99998554 , 0.00213018,
+                                              -0.04360654 ,-0.00234354,  0.99904603  );
+cv::Mat D2_P2 = (cv::Mat_<float>(3,4) <<     216.85273609    ,0.     ,     132.90382004, -494.29453342,
+                                            0.    ,      216.85273609 , 105.3395071  ,   0.        ,
+                                             0.        ,   0.        ,   1.        ,   0.  );
+
+
+
+
+
+// Image Rectification Values D3
+cv::Mat D3_cameraMatrix_left = (cv::Mat_<float>(3,3) << 216.0422918 ,   0.        , 161.25731835,
+                                              0.        , 216.0422918 ,  89.79627252,
+                                              0.        ,   0.        ,   1.     );
+cv::Mat D3_distortionCoefficients_left = (cv::Mat_<float>(1,5) << -0.01682369,  0.22907142,  0.        ,  0.        , -0.39213378);
+cv::Mat D3_R1 = (cv::Mat_<float>(3,3) <<        0.99980977,  0.01748846, -0.00863564,
+                                             -0.01763787,  0.99969059, -0.01753911,
+                                               0.00832624,  0.01768808,  0.99980888  );
+cv::Mat D3_P1 = (cv::Mat_<float>(3,4) <<        216.0422918 ,   0.        , 165.85542297, 0, 
+                                             0.        , 216.0422918 ,  99.9402914 ,   0. ,
+                                             0.        ,   0.        ,   1.        ,   0.  );
+cv::Mat D3_cameraMatrix_right = (cv::Mat_<float>(3,3) << 216.0422918 ,   0.        , 151.88891564,
+                                                    0.        , 216.0422918 , 104.14842486,
+                                                    0.        ,   0.        ,   1.     );
+cv::Mat D3_distortionCoefficients_right = (cv::Mat_<float>(1,5) << -0.00733585,  0.10949051,  0.        ,  0.        , -0.182561238);
+cv::Mat D3_R2 = (cv::Mat_<float>(3,3) <<        0.99954373,  0.02270267, -0.01992291,
+                                             -0.02234817,  0.99959108,  0.01783911,
+                                              0.02031975, -0.01738573,  0.99964236  );
+cv::Mat D3_P2 = (cv::Mat_<float>(3,4) <<        216.0422918 ,    0.        ,  155.36833954, -990.02692196,
+                                            0.        ,  216.0422918 ,   99.9402914 ,    0.      ,
+                                             0.        ,   0.        ,   1.        ,   0.  );
+
+
+
+
+void set_rmap_calibration_matrices(){
+
+    // GET STEREORECTIFICATION REMAP MATRICES
+    // Stereorectification Maps for D1
+    cv::initUndistortRectifyMap(D1_cameraMatrix_left, D1_distortionCoefficients_left,   D1_R1, D1_P1, cv::Size(img_width*img_scale, img_height) ,  CV_16SC2, disp1_rmap[0][0], disp1_rmap[0][1]);
+    cv::initUndistortRectifyMap(D1_cameraMatrix_right, D1_distortionCoefficients_right, D1_R2, D1_P2, cv::Size(img_width*img_scale ,img_height), CV_16SC2,   disp1_rmap[1][0], disp1_rmap[1][1]);
+    // Stereorectification Maps for D2
+    cv::initUndistortRectifyMap(D2_cameraMatrix_left, D2_distortionCoefficients_left,   D2_R1, D2_P1, cv::Size(img_width*img_scale, img_height) ,  CV_16SC2, disp2_rmap[0][0], disp2_rmap[0][1]);
+    cv::initUndistortRectifyMap(D2_cameraMatrix_right, D2_distortionCoefficients_right, D2_R2, D2_P2, cv::Size(img_width*img_scale ,img_height), CV_16SC2,   disp2_rmap[1][0], disp2_rmap[1][1]);
+    // Stereorectification Maps for D3
+    cv::initUndistortRectifyMap(D3_cameraMatrix_left, D3_distortionCoefficients_left,   D3_R1, D3_P1, cv::Size(img_width*img_scale, img_height) ,  CV_16SC2, disp3_rmap[0][0], disp3_rmap[0][1]);
+    cv::initUndistortRectifyMap(D3_cameraMatrix_right, D3_distortionCoefficients_right, D3_R2, D3_P2, cv::Size(img_width*img_scale ,img_height), CV_16SC2,   disp3_rmap[1][0], disp3_rmap[1][1]);
+
+}
+
+
+
+
+void getDisparity1(cv::Mat &disp_imgL, cv::Mat &disp_imgR, cv::Mat &disp, int edge_max){
+        
+        cv::Mat imgL_rect1; 
+        cv::Mat imgR_rect1; 
+        
+        cv::remap(disp_imgL, imgL_rect1, disp1_rmap[0][0], disp1_rmap[0][1], cv::INTER_LINEAR, cv::BORDER_DEFAULT, cv::Scalar());
+        cv::remap(disp_imgR, imgR_rect1, disp1_rmap[1][0], disp1_rmap[1][1], cv::INTER_LINEAR, cv::BORDER_DEFAULT, cv::Scalar());
+
+        cv::resize(imgL_rect1, imgL_rect1, cv::Size(),postRectificationRescale, postRectificationRescale); 
+        cv::resize(imgR_rect1, imgR_rect1, cv::Size(),postRectificationRescale,postRectificationRescale); 
+
+        elas1_(imgL_rect1, imgR_rect1, disp, edge_max); 
+        
+        disp =  (disp/16);
+        cv::flip(disp, disp, 1);
+        disp.convertTo(disp, CV_8U); 
+
+}
+
+
+void getDisparity2(cv::Mat &disp_imgL, cv::Mat &disp_imgR, cv::Mat &disp, int edge_max){
+
+        cv::Mat imgL_rect2; 
+        cv::Mat imgR_rect2; 
+
+        cv::remap(disp_imgL, imgL_rect2, disp2_rmap[0][0], disp2_rmap[0][1], cv::INTER_LINEAR, cv::BORDER_DEFAULT, cv::Scalar());
+        cv::remap(disp_imgR, imgR_rect2, disp2_rmap[1][0], disp2_rmap[1][1], cv::INTER_LINEAR, cv::BORDER_DEFAULT, cv::Scalar());
+
+        cv::resize(imgL_rect2, imgL_rect2, cv::Size(), postRectificationRescale, postRectificationRescale); 
+        cv::resize(imgR_rect2, imgR_rect2, cv::Size(), postRectificationRescale, postRectificationRescale); 
+
+        elas2_(imgL_rect2, imgR_rect2, disp, edge_max); 
+        
+        disp =  (disp/16);
+        cv::flip(disp, disp, 1);
+        disp.convertTo(disp, CV_8U); 
+
+} 
+
+void getDisparity3(cv::Mat &disp_imgL, cv::Mat &disp_imgR, cv::Mat &disp, int edge_max){
+
+        cv::Mat imgL_rect3; 
+        cv::Mat imgR_rect3; 
+        
+        cv::remap(disp_imgL, imgL_rect3, disp3_rmap[0][0], disp3_rmap[0][1], cv::INTER_LINEAR, cv::BORDER_DEFAULT, cv::Scalar());
+        cv::remap(disp_imgR, imgR_rect3, disp3_rmap[1][0], disp3_rmap[1][1], cv::INTER_LINEAR, cv::BORDER_DEFAULT, cv::Scalar());
+
+        cv::resize(imgL_rect3, imgL_rect3, cv::Size(), postRectificationRescale, postRectificationRescale); 
+        cv::resize(imgR_rect3, imgR_rect3, cv::Size(), postRectificationRescale, postRectificationRescale); 
+
+        elas3_(imgL_rect3, imgR_rect3, disp, edge_max);
+
+        disp =  (disp/16);
+        cv::flip(disp, disp, 1);
+        disp.convertTo(disp, CV_8U); 
+}
+
+void getD3hat(cv::Mat &disp1, cv::Mat &disp2, cv::Mat &disp3_hat){
+    disp3_hat.setTo(cv::Scalar(0)); 
+    int height = img_height*postRectificationRescale; 
+    int width = img_width/4*postRectificationRescale; 
+
+    int j_hat; 
+
+    for (int i=0; i<height; i++){
+        for(int j=0; j+rectification_shift2_3<width; j++){
+            j_hat = j + disp1.at<uint8_t>(i,j); 
+            if (j_hat + rectification_shift1_2< width){ 
+
+                int d2 = disp2.at<uint8_t>(i,j_hat + rectification_shift1_2); 
+                int d1 = j_hat-j;
+
+                disp3_hat.at<uint8_t>(i,j+rectification_shift2_3) = d2 + d1; 
+            }
+        }
+    }
+
+//     for (int i=0; i<height; i++){
+//         for(int j=0; j<width; j++){
+//             j_hat = j + disp2.at<uint8_t>(i,j); 
+//             if (j_hat < width){ 
+
+//                 int d1 = disp1.at<uint8_t>(i,j_hat); 
+//                 int d2 = j_hat-j;
+
+//                 disp3_hat.at<uint8_t>(i,j) = d2 + d1; 
+//             }
+//         }
+//     }
+
+
+}
+
+
+
+void initErrTensor(Eigen::Tensor<float,3, Eigen::RowMajor> &et){
+    // Set the error tensor with a decaying exponential that sums to 1 
+    for (int i=0; i<img_height*postRectificationRescale; ++i){
+        for (int j=0; j<frame_width*postRectificationRescale; ++j){
+            for (int k=0; k<err_depth; ++k){
+                et(i,j,k) = exp(-k)/1.5819767068693267;
+            }
+        }
+    }   
+}
+
+void get_max_err(const Eigen::Tensor<float,3, Eigen::RowMajor> &et, cv::Mat &maxErr){
+    maxErr.setTo(cv::Scalar(0));
+    for (int i=0; i<img_height*postRectificationRescale; ++i){
+        for (int j=0; j<frame_width*postRectificationRescale; ++j){
+            float prob_sum = et(i,j,err_depth-1);
+            for (int k=0; k<err_depth; ++k){
+                prob_sum += et(i, j, k);
+                if (prob_sum > 0.975){
+                    maxErr.at<uint8_t>(i, j) = k;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+ void view_slice(const Eigen::Tensor<float,3, Eigen::RowMajor> &et, int slice_num){
+     cv::Mat slice( img_height*postRectificationRescale, frame_width*postRectificationRescale, CV_8U, cv::Scalar(0));
+     for (int i=0; i<img_height*postRectificationRescale; ++i){
+         for (int j=0; j<frame_width*postRectificationRescale; ++j){
+            slice.at<uint8_t>(i,j) = 255*et(i,j,slice_num);
+         }
+     }
+//     slice.convertTo(slice, CV_8UC3);
+//     cv::applyColorMap(slice, slice, cv::COLORMAP_JET);
+     std::string winname = "Slice ";
+
+     cv::imshow(winname + std::to_string(slice_num), slice);
+     cv::waitKey(5);
+
+ }
---
+++ uncertaintyestimator_on_laptop/include/cbf.h
@@ -0,0 +1,246 @@
+#include <ecos.h>
+#include <Eigen/SparseCore>
+
+#define PI 3.14159265
+
+
+// Norm Ball Barrier Function 
+// float alpha = 0.15; 
+// float C = 0.5; 
+// float beta = 1; 
+
+// Norm ball with heading
+float alpha = 0.3; 
+float C = 0.33; 
+float beta = 0.1; 
+
+float ballCBF(Eigen::Vector3f rho){
+    return 1.0/2*(pow(rho.norm(),2)-pow(C,2)); 
+}
+
+float CBF(Eigen::Vector3f rho){
+    return 1.0/2*(pow(rho.norm(),2) - pow(C,2)  - beta*( pow((PI/4), 2) - pow(atan(rho[0]/rho[2]),2))   );
+}
+
+
+void argmin_cbf(std::vector<float> k_des, Eigen::Vector3f rho , std::vector<float> &u_star){
+    // double lower_bound = -alpha*CBF(rho)/(-rho[2]); 
+    // if (k_des[0] < lower_bound){
+    //     return k_des[0]; 
+    // }else{
+    //     return lower_bound; 
+    // }
+
+    Eigen::Vector2f Lgh = {-rho[2] + beta*atan(rho[0]/rho[2])*rho[0]/(pow(rho[2],2) + pow(rho[0],2)), -beta*atan(rho[0]/rho[2])};
+    
+    double ineq = Lgh[0]*k_des[0] + Lgh[1]*k_des[1] + alpha*CBF(rho);
+    if (ineq > 0 ){
+        u_star[0] = k_des[0]; 
+        u_star[1] = k_des[1]; 
+    }else{
+        u_star[0]  = -Lgh[0]*alpha*CBF(rho)/(Lgh.dot(Lgh));
+        u_star[1]  = -Lgh[1]*alpha*CBF(rho)/(Lgh.dot(Lgh));
+    }
+}
+
+
+// ECOS Type Defs 
+typedef Eigen::SparseMatrix<double, Eigen::ColMajor> SpMat; // declares a column-major sparse matrix type of double
+typedef Eigen::Triplet<double> T;
+
+
+// ECOS PARAMS 
+idxint n = 4; // number of decision vars
+idxint p = 0; // number of equality constraints
+idxint l = 0; // number of positive orthant constriants
+idxint e = 0; // number of exponential cones 
+pfloat c[4] = {1.0, 0.0, 0, 0};
+
+// Uncertainty Params
+int Lfh = 0; 
+double L_lgh = 0; 
+double err_epsilon = 0; 
+double vel_weight = 5;
+
+void argmin_cbf_ecos(std::vector<float> k_des, Eigen::Vector3f rho , std::vector<float> &u_star){
+    // ECOS Params 
+    idxint m = 7; // number of constraints
+    idxint ncones = 2; // number of second order cones 
+    idxint q[2] = {4, 3}; // Array of the length of each of the ncones 
+
+    Eigen::Vector2f Lgh = {-rho[2] + beta*atan(rho[0]/rho[2])*rho[0]/(pow(rho[2],2) + pow(rho[0],2)), -beta*atan(rho[0]/rho[2])};
+
+    c[2] = -pow(vel_weight,2)*k_des[0]; 
+    c[3] = -k_des[1]; 
+    pfloat h_ecos[7] = {1/sqrt(2), -1/sqrt(2), 0.0, 0.0, alpha*CBF(rho) + Lfh , 0.0, 0.0};
+
+	SpMat G(7,4); 
+    std::vector<T> tripletList;
+    tripletList.push_back(T(0,0,-1.0/sqrt(2))); 
+	tripletList.push_back(T(1,0,-1.0/sqrt(2))); 
+	tripletList.push_back(T(2,2,-1*vel_weight)); 
+	tripletList.push_back(T(3,3,-1)); 
+	tripletList.push_back(T(4,2,-Lgh[0])); 
+	tripletList.push_back(T(4,3,-Lgh[1])); 
+	tripletList.push_back(T(5,2,-L_lgh*err_epsilon)); 
+	tripletList.push_back(T(6,3,-L_lgh*err_epsilon)); 
+    G.setFromTriplets(tripletList.begin(), tripletList.end()); 
+	G.makeCompressed();
+	double *Gpr = G.valuePtr(); 
+	idxint Gir[8];
+	idxint Gjc[5]; 
+
+    // Convert Type
+	for(int i=0; i<8; i++){
+		Gir[i] = (long)G.innerIndexPtr()[i]; 
+	}
+	for(int i=0; i<5; i++){
+		Gjc[i] = (long)G.outerIndexPtr()[i]; 
+	}
+
+    pwork *ecos_problem; 
+	ecos_problem = ECOS_setup(n, m, p, l, ncones, q, e, Gpr, Gjc, Gir, NULL, NULL, NULL, c, h_ecos, NULL);
+
+    if (ecos_problem == NULL){
+        std:: cout << "Ecos setup problem " << std::endl; 
+    }
+
+	idxint ecos_flag = ECOS_solve(ecos_problem);
+
+    if (ecos_flag == 0 || ecos_flag == 10){
+        u_star[0] = ecos_problem->x[2]; 
+        u_star[1] = ecos_problem->x[3]; 
+    }else{
+        std::cout << "ECOS Problem " << std::endl; 
+        std::cout << "ECOS flag = " << ecos_flag << std::endl;
+    }
+
+    ECOS_cleanup(ecos_problem, 0); 
+
+}
+
+
+
+void argmin_cbf_ecos_multiple_points(const std::vector<float> & k_des,
+                                     const std::vector<Eigen::Vector3f> & rhos ,
+                                     std::vector<float> &u_star){
+
+    idxint m = 4 + 3*rhos.size(); // Change number of constraints, 4 for the rotated cone and 3 for each CBF 
+    idxint ncones = 1 + rhos.size(); // number of second order cones, the rotated cone + one for each point
+    
+    idxint* q = NULL; 
+    pfloat* h_ecos = NULL; 
+
+    q = new idxint[ncones]; 
+    h_ecos = new pfloat[m]; 
+
+    for (int i=0; i<ncones; i++){ 
+        if (i==0){ 
+            q[i] = 4; 
+        }else{
+            q[i] = 3; 
+        }
+    }
+
+
+
+
+    c[2] = -pow(vel_weight,2)*k_des[0]; 
+    c[3] = -k_des[1]; 
+    h_ecos[0] = 1/sqrt(2);
+    h_ecos[1] = -1/sqrt(2); 
+    h_ecos[2] = 0.0;
+    h_ecos[3] = 0.0; 
+
+
+    SpMat G(m,4); 
+    std::vector<T> tripletList;
+    tripletList.push_back(T(0,0,-1.0/sqrt(2))); 
+	tripletList.push_back(T(1,0,-1.0/sqrt(2))); 
+	tripletList.push_back(T(2,2,-1*vel_weight)); 
+	tripletList.push_back(T(3,3,-1)); 
+
+    for(size_t rhs_idx=0; rhs_idx < rhos.size(); rhs_idx++){
+
+        double xhat = rhos[rhs_idx][2]; 
+        double yhat = rhos[rhs_idx][0];
+        // Eigen::Vector2f Lgh = {-xhat + beta*atan(yhat/xhat)*yhat/(pow(xhat,2) + pow(yhat,2)), -beta*atan(yhat/xhat)}; 
+        Eigen::Vector2f Lgh = {-1, 0};
+
+        // h_ecos[4 + 3*rhs_idx] = alpha*CBF(rhos[rhs_idx]); 
+        h_ecos[4 + 3*rhs_idx] = alpha*ballCBF(rhos[rhs_idx])/xhat; 
+        h_ecos[4 + 3*rhs_idx + 1] = 0; 
+        h_ecos[4 + 3*rhs_idx + 2] = 0; 
+
+        tripletList.push_back(T(4 + 3*rhs_idx,2,-Lgh[0])); 
+        tripletList.push_back(T(4 + 3*rhs_idx,3,-Lgh[1])); 
+        tripletList.push_back(T(4 + 3*rhs_idx + 1,2,-L_lgh*err_epsilon)); 
+        tripletList.push_back(T(4 + 3*rhs_idx + 2,3,-L_lgh*err_epsilon)); 
+    }
+
+
+
+
+    G.setFromTriplets(tripletList.begin(), tripletList.end()); 
+	G.makeCompressed();
+	double *Gpr = G.valuePtr(); 
+	idxint Gir[m + rhos.size()]; // 1 for each constraint row plus an extra for each row with Lgh (of which there's one per every rho)
+	idxint Gjc[5]; 
+
+    // Convert Type
+	for(size_t i=0; i<m + rhos.size(); i++){
+		Gir[i] = (long)G.innerIndexPtr()[i]; 
+	}
+	for(int i=0; i<5; i++){
+		Gjc[i] = (long)G.outerIndexPtr()[i]; 
+	}
+
+    // std::cout << G << std::endl; 
+
+    pwork *ecos_problem; 
+	ecos_problem = ECOS_setup(n, m, p, l, ncones, q, e, Gpr, Gjc, Gir, NULL, NULL, NULL, c, h_ecos, NULL);
+
+    if (ecos_problem == NULL){
+        std:: cout << "Ecos setup problem " << std::endl; 
+    }
+
+	idxint ecos_flag = ECOS_solve(ecos_problem);
+
+    if (ecos_flag == 0 || ecos_flag == 10){
+        u_star[0] = ecos_problem->x[2]; 
+        u_star[1] = ecos_problem->x[3]; 
+    }else{
+        std::cout << "ECOS Problem " << std::endl; 
+        std::cout << "ECOS flag = " << ecos_flag << std::endl;
+    }
+
+    ECOS_cleanup(ecos_problem, 0); 
+
+    delete [] q; 
+    q = NULL; 
+    delete [] h_ecos; 
+    h_ecos = NULL; 
+
+}
+
+
+
+
+// double get_L_lgh(Eigen::Vector3f rho, int dmin){ 
+//     double du = rho[0]/rho[2]*focus; 
+//     double dv = rho[1]/rho[2]*focus; 
+//     double phi = -beta*dv/2/(pow(dv, 2) + pow(du,2)); 
+
+//     Eigen::Vector2f dLgh = {du*baseline/pow(dmin, 2) + phi, phi }; 
+//     // Eigen::Vector2f dLgh = 0; //{du*baseline/pow(dmin, 2) + phi, phi }; 
+
+//     return dLgh.norm(); 
+// }
+
+
+// double get_L_ah(Eigen::Vector3f rho, int dmin){
+//     double du = rho[0]/rho[2]*focus; 
+//     double dv = rho[1]/rho[2]*focus; 
+//     return abs(-pow(baseline,2)/pow(dmin,3)*(pow(du,2) + pow(dv,2)));
+//     // return alpha * ball / xhat 
+// }
